0d7611b8a6b15cf374ed60b118beb638d7440137
Kyle Uehlein <kyleuehlein@gmail.com>
2017-12-12 14:57:26 -0500

    initial commit

---
+++ vobe.js
@@ -0,0 +1,47 @@
+'use strict'
+
+var _ = require('lodash')
+var reactRedux = require('react-redux')
+var connect = reactRedux.connect
+
+
+function vobeReducer() {
+  var modelTypes = Array.prototype.slice.call(arguments)
+  var reducer = function(action) {
+    return reducer[action] ? action : modelTypes[0]
+  }
+
+  // throw an error if vobeReducer doesn't have the necessary arguments
+  if (!modelTypes.length) {
+    throw new Error('There is a vobeReducer without any arguments. You need to provide your vobeReducer with a default model type and supplementary model types.')
+  } else if (modelTypes.length < 2) {
+    throw new Error('There is a vobeReducer with a default model type, but no additional model types. You need to provide your vobeReducer with supplementary model types.')
+  }
+
+  // add actions onto reducer, used to dispatch
+  else {
+    modelTypes.forEach(function(type) {
+
+      reducer[type] = function(componentModel) {
+        var newState = _.cloneDeep(type)
+
+        if (!componentModel || !componentModel.length) throw new Error('There is a model dispatch without any arguments provided. You need to provide your model dispatch with an array of components. A model dispatch is the function returned by a vobeReducer')
+
+        newState.components = componentModel
+
+        return function(actionToDispatch) {
+          if (actionToDispatch) dispatch(actionToDispatch)
+          dispatch(newState)
+        }
+      }
+    })
+
+    return reducer
+  }
+}
+connect()(vobeReducer)
+
+
+module.exports = {
+  vobeReducer
+}
---
+++ vobe.spec.js
@@ -0,0 +1,3 @@
+'use strict'
+
+// tests for vobe...
